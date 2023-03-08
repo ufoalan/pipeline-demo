9994e351e9afe16dc67e11eef34067fe425d8ada
Alan Lee <siulee@redhat.com>
2023-03-08 15:32:33 +1100

    fix image name error

--- create_helm_chart.sh
+++ create_helm_chart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-CHART_NAME=$1
+SVC_NAME=$1
+CHART_NAME=$SVC_NAME'-chart'
 CHART_VERSION=$2
 REPO=$3
 ORG_NAME=$4
@@ -8,7 +9,7 @@ CHART_HOME=/opt/homebrew/bin
 echo "Creating $CHART_NAME $CHART_VERSION $REPO $ORG_NAME $DEPLOY_ENV"
 $CHART_HOME/helm create $CHART_NAME
 # replace the image name by your own image name
-sed -i .bak "s/repository: nginx/repository: $REPO\/$ORG_NAME\/$CHART_NAME/g" $CHART_NAME/values.yaml
+sed -i .bak "s/repository: nginx/repository: $REPO\/$ORG_NAME\/$SVC_NAME/g" $CHART_NAME/values.yaml
 sed -i .bak 's/port: 80/port: 8080/g' $CHART_NAME/values.yaml
 sed -i .bak "s/1.16.0/$CHART_VERSION/g" $CHART_NAME/Chart.yaml
 $CHART_HOME/helm template $CHART_NAME
