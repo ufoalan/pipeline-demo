8193fa6c4218a6b419a96e52c3fe5aed73c23af8
Alan Lee <siulee@redhat.com>
2023-03-08 13:26:34 +1100

    updated create_helm_chart.sh

--- create_helm_chart.sh
+++ create_helm_chart.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 CHART_NAME=$1
 CHART_VERSION=$2
-echo "hello $CHART_NAME $CHART_VERSION"
-helm create $CHART_NAME
+CHART_HOME=/opt/homebrew/bin
+echo "Creating $CHART_NAME $CHART_VERSION"
+$CHART_HOME/helm create $CHART_NAME
 # replace the image name by your own image name
 sed -i .bak "s/repository: nginx/repository: $CHART_NAME/g" $CHART_NAME/values.yaml
 sed -i .bak 's/port: 80/port: 8080/g' $CHART_NAME/values.yaml
 sed -i .bak "s/1.16.0/$CHART_VERSION/g" $CHART_NAME/Chart.yaml
-helm template $CHART_NAME
+$CHART_HOME/helm template $CHART_NAME
