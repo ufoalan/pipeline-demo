0cee9549a382b0b92b9f6556ebfdc51a15cb8dc8
Alan Lee <siulee@redhat.com>
2023-03-08 14:51:46 +1100

    modified create_helm_chart.sh to accept 2 more parameters

--- create_helm_chart.sh
+++ create_helm_chart.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 CHART_NAME=$1
 CHART_VERSION=$2
-DEPLOY_ENV=$3
+REPO=$3
+ORG_NAME=$4
+DEPLOY_ENV=$5
 CHART_HOME=/opt/homebrew/bin
-echo "Creating $CHART_NAME $CHART_VERSION"
+echo "Creating $CHART_NAME $CHART_VERSION $REPO $ORG_NAME $DEPLOY_ENV"
 $CHART_HOME/helm create $CHART_NAME
 # replace the image name by your own image name
-sed -i .bak "s/repository: nginx/repository: $CHART_NAME/g" $CHART_NAME/values.yaml
+sed -i .bak "s/repository: nginx/repository: $REPO\/$ORG_NAME\/$CHART_NAME/g" $CHART_NAME/values.yaml
 sed -i .bak 's/port: 80/port: 8080/g' $CHART_NAME/values.yaml
 sed -i .bak "s/1.16.0/$CHART_VERSION/g" $CHART_NAME/Chart.yaml
 $CHART_HOME/helm template $CHART_NAME
-$CHART_HOME/helm upgrade -i $CHART_NAME $CHART_NAME -n $DEPLOY_ENV
+#$CHART_HOME/helm upgrade -i $CHART_NAME $CHART_NAME -n $DEPLOY_ENV
