73b2446e7c1a97fb3005c1d1293866a29216f5f5
Alan Lee <siulee@redhat.com>
2023-03-08 14:32:04 +1100

    modified create_helm_chart.sh to add helm deployment command

--- create_helm_chart.sh
+++ create_helm_chart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CHART_NAME=$1
 CHART_VERSION=$2
+DEPLOY_ENV=$3
 CHART_HOME=/opt/homebrew/bin
 echo "Creating $CHART_NAME $CHART_VERSION"
 $CHART_HOME/helm create $CHART_NAME
@@ -9,3 +10,4 @@ sed -i .bak "s/repository: nginx/repository: $CHART_NAME/g" $CHART_NAME/values.y
 sed -i .bak 's/port: 80/port: 8080/g' $CHART_NAME/values.yaml
 sed -i .bak "s/1.16.0/$CHART_VERSION/g" $CHART_NAME/Chart.yaml
 $CHART_HOME/helm template $CHART_NAME
+$CHART_HOME/helm upgrade -i $CHART_NAME $CHART_NAME -n $DEPLOY_ENV
