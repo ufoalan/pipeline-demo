bfcffebc2706ffd952367d60013b76683d69f5fc
Alan Lee <siulee@redhat.com>
2023-03-08 14:56:49 +1100

    uncomment the deployment command

--- create_helm_chart.sh
+++ create_helm_chart.sh
@@ -12,4 +12,4 @@ sed -i .bak "s/repository: nginx/repository: $REPO\/$ORG_NAME\/$CHART_NAME/g" $C
 sed -i .bak 's/port: 80/port: 8080/g' $CHART_NAME/values.yaml
 sed -i .bak "s/1.16.0/$CHART_VERSION/g" $CHART_NAME/Chart.yaml
 $CHART_HOME/helm template $CHART_NAME
-#$CHART_HOME/helm upgrade -i $CHART_NAME $CHART_NAME -n $DEPLOY_ENV
+$CHART_HOME/helm upgrade -i $CHART_NAME $CHART_NAME -n $DEPLOY_ENV
