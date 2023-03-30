7668645a9f40424247d3fbcc224812356e1ae635
Alan Lee <siulee@redhat.com>
2023-03-30 16:48:29 +1100

    add new tls-client nodejs module. This module is to test the SSL HTTP server endpoint using 2 way SSL

---
+++ tls-client/tls-client.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+'use strict';
+
+const port = 8000;
+const hostname = 'localhost';
+
+const tls = require('tls');
+const fs = require('fs');
+
+const options = {
+  host: hostname,
+  port: port,
+
+  // Necessary only if using the client certificate authentication
+//  key: fs.readFileSync('certs/client/client.key'),
+//  cert: fs.readFileSync('certs/client/client.crt'),
+  key: fs.readFileSync('certs/client/tls.key'),
+  cert: fs.readFileSync('certs/client/tls.crt'),
+
+  // Necessary only if the server uses the self-signed certificate
+  ca: fs.readFileSync('certs/ca/tls.crt')
+};
+
+const socket = tls.connect(options, () => {
+  console.log('client connected', socket.authorized ? 'authorized' : 'unauthorized');
+  if (!socket.authorized) {
+    console.log("Error: ", socket.authorizationError());
+    socket.end();
+  }
+})
+  .setEncoding('utf8')
+  .on('data', (data) => {
+    console.log("Received: ", data);
+
+    // Close after receive data
+    socket.end();
+  })
+  .on('close', () => {
+    console.log("Connection closed");
+  })
+  .on('end', () => {
+    console.log("End connection");
+  })
+  .on('error', (error) => {
+    console.error(error);
+    socket.destroy();
+  });
